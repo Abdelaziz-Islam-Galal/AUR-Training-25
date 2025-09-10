f176ff2ee49e20db083ca9182222f69c517676f2
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-09-10 10:17:06 +0300

    added file names + task 1

---
+++ Phase 2/Session 5/ros2_ws/src/pkg_py/pkg_py/timer node.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+class timer(Node):
+    def __init__(self):
+        super().__init__("timer")
+        # self.get_logger().info("Started timer node")
+        self.counter = 10
+        if self.counter >= 0:
+            self.create_timer(1, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info(f"{self.counter}")
+        self.counter -= 1
+        if self.counter == 0:
+            self.get_logger().info("Time is up!")
+
+def main():
+    rclpy.init()
+    node = timer()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+                
