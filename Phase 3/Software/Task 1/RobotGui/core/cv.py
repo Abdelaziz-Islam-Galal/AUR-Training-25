798ae2888234967e663b45c93f8a1509a2ff0290
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-04 13:47:16 +0300

    applied threading to the main code

--- Phase 3/Software/Task 1/RobotGui/core/cv.py
+++ Phase 3/Software/Task 1/RobotGui/core/cv.py
@@ -1,5 +1,6 @@
 import cv2
-from threading import Thread
+from threading import 
+from time import sleep
 # from pathlib import Path
 # import numpy as np
 
@@ -18,6 +19,14 @@ class Camera:
         self._capture_device = cv2.VideoCapture()
         self._capture_device.open(CAPTURE_DEVICE)
 
+        # Session 2 continouing the code:
+        self._empty_frame = cv2.imread('cat pics/felfel.jpg')
+        self._frame = None
+        self._frame_thread = Thread(target = self._frame_loop, daemon = True) # daemon = True so when window is closed it does not wait for this thread to finish
+        self._frame_thread.start()
+        # Note:
+        # The thread asks for a frame before the device connects and sends its frame; hence, we used an empty_frame to fill in this time
+
     @property
     def frame(self):
         # Original Code (to display cat pics):
@@ -28,14 +37,34 @@ class Camera:
         
         # Task 1 Code:
 
-        capture = self._capture_device.read()
-        # capture is a typle (bool isSuccess, frame array)
-        if capture[0]:
-            return capture[1]
-        else:
-            print("an error in capturing a frame occured!")
+        # capture = self._capture_device.read()
+        # # capture is a typle (bool isSuccess, frame array)
+        # if capture[0]:
+        #     return capture[1]
+        # else:
+        #     print("an error in capturing a frame occured!")
             
-            # Close and Open device agian because the problem may be in the camera itself
-            self._capture_device.release()
-            self._capture_device.open(CAPTURE_DEVICE)
+        #     # Close and Open device agian because the problem may be in the camera itself
+        #     self._capture_device.release()
+        #     self._capture_device.open(CAPTURE_DEVICE)
 
+        # Session 2 Code: -> applied threading so capturing images does not slow down the gui in the main thread
+
+        return self._frame if self._frame is not None else self._empty_frame
+
+    # for Session 2 Code
+    def _frame_loop(self):
+        while True:
+            success, image = self._capture_device.read()
+            # capture is a typle (bool isSuccess, frame array)
+            if success:
+                self._frame = image
+            else:
+                print("an error in capturing a frame occured!")
+                # Close and Open device agian because the problem may be in the camera itself
+                self._capture_device.release()
+                self._capture_device.open(CAPTURE_DEVICE)
+
+            sleep(0.015) # 15 ms delay so this function is a bit slower so it does not consume most of the cpu power
+            # we chose 15 ms while the frames update every 50 ms (we chose that value in function update)
+            # so if a problem occurs it can be detected before the next frame is sent
