157bc9c96f4c234eb836f62863f471f9e1b338b4
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-04 12:05:32 +0300

    further improvments from session 2 video

--- Phase 3/Software/Task 1/RobotGui/gui/camera_display.py
+++ Phase 3/Software/Task 1/RobotGui/gui/camera_display.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QLabel, QSizePolicy
-from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtGui import QImage, QPixmap, QResizeEvent
+from PySide6.QtCore import QTimer
 from RobotGui.core.cv import Camera
 
 class CameraDisplay(QWidget):
@@ -12,6 +13,20 @@ class CameraDisplay(QWidget):
         self._frame_view.setScaledContents(True)
 
         self.update_view()
+
+        # Timer to update frames (makes it like a video)
+        self._camera_timer = QTimer()
+        self._camera_timer.timeout.connect(self.update_view())
+        self._camera_timer.setInterval(50)
+        self._camera_timer.start()
+
+    def resizeEvent(self, event: QResizeEvent) -> None: 
+        super().resizeEvent(event)
+        # that is a function in the QWidget Class, I overwrite it so the CameraDisplay window
+        # is dynamic and gets resized with the main window
+
+        self._frame_view.resize(event.size())
+        # I am giving the _frame_view parameter the size of the event where the event is the main window that I resize
 
     def update_view(self):
         frame = self._camera_device.frame
--- Phase 3/Software/Task 1/RobotGui/gui/window.py
+++ Phase 3/Software/Task 1/RobotGui/gui/window.py
@@ -1,19 +1,14 @@
 from PySide6.QtWidgets import QWidget, QMainWindow
-from PySide6.QtCore import QTimer
 from RobotGui.gui.camera_display import CameraDisplay
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(640, 480) # fixed window size in pixels I think
+        self.setMinimumSize(640, 480) # fixed window size in pixels I think
 
         self._camera_widget = CameraDisplay(self) # initialising the object that will display what the camera will capture
         self.setCentralWidget(self._camera_widget) # where to show the captured images
 
         self.show() # window containing the camera display appear
 
-        # Timer to update frames (makes it like a video)
-        self._camera_timer = QTimer()
-        self._camera_timer.timeout.connect(self._camera_widget.update_view)
-        self._camera_timer.setInterval(50)
-        self._camera_timer.start()
+        
